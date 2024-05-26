fa69d93d7bccccd752e1a5db6a9c32f4b0663c8c
andero-magi <andero.magi.420+schoolgit@gmail.com>
2024-05-26 17:55:58 +0300

    Use DateTime instead of DateOnly in SickLeave

--- Models/SickLeave.cs
+++ Models/SickLeave.cs
@@ -9,9 +9,9 @@ namespace FirmaDB_Migration.Models
 
     public Employee Employee { get; set; }
 
-    public DateOnly StartDate { get; set; }
+    public DateTime StartDate { get; set; }
 
-    public DateOnly EndDate { get; set; }
+    public DateTime EndDate { get; set; }
 
     public int Days { get; set; }
 
